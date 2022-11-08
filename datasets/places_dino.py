b577734e9adfb4f96892b2ac77a7d45a8c41acbc
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-11-08 16:05:51 +0100

    chore: more linting

--- datasets/places_dino.py
+++ datasets/places_dino.py
@@ -13,8 +13,8 @@ class Dataset(BaseDataset):
     name = "dino_places"
     is_sparse = False
 
-    x_url = "http://pascal.inrialpes.fr/data2/mairal/data/feat_PLACES_train.npy"
-    y_url = "http://pascal.inrialpes.fr/data2/mairal/data/lab_PLACES_train.npy"
+    x_url = "http://pascal.inrialpes.fr/data2/cyanure/datasets/feat_PLACES_train.npy"
+    y_url = "http://pascal.inrialpes.fr/data2/cyanure/datasets/lab_PLACES_train.npy"
 
     def __init__(self):
         self.X, self.y = None, None
@@ -22,10 +22,10 @@ class Dataset(BaseDataset):
     def get_data(self):
         if self.X is None:
             cachedir = os.path.dirname(benchopt.__file__) + os.path.sep + "cache"
-            path_X = download(self.x_url, os.path.join(cachedir, 
-                                "feat_PLACES_train.npy"))
-            path_y = download(self.y_url, os.path.join(cachedir, 
-                                "lab_PLACES_train.npy"))
+            path_X = download(self.x_url, os.path.join(cachedir,
+                            "feat_PLACES_train.npy"))
+            path_y = download(self.y_url, os.path.join(cachedir,
+                            "lab_PLACES_train.npy"))
             self.X = np.load(os.path.join(path_X), allow_pickle=True)
             self.y = np.load(os.path.join(path_y), allow_pickle=True)
             self.y = np.squeeze(self.y)
