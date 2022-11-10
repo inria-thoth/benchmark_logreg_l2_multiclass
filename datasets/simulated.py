3211f40bb33f0e745157cbcac09d3bc1d380186c
Thomas Ryckeboer <37658138+thomasryck@users.noreply.github.com>
2022-11-10 11:40:35 +0100

    fix: simulated l2

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -7,27 +7,29 @@ class Dataset(BaseDataset):
 
     name = "Simulated"
 
-    # List of parameters to generate the datasets. The benchmark will consider
-    # the cross product for each key in the dictionary.
     parameters = {
         'n_samples, n_features': [
-            (1000, 500),
-            (5000, 200)]
+            (200, 500),
+            (1000, 10),
+            (100_000, 400),
+        ]
     }
 
-    def __init__(self, n_samples=10, n_features=50, random_state=27):
-        # Store the parameters of the dataset
+    def __init__(self, n_samples=10, n_features=50, random_state=42):
         self.n_samples = n_samples
         self.n_features = n_features
         self.random_state = random_state
 
     def get_data(self):
-
         rng = np.random.RandomState(self.random_state)
-        X = rng.randn(self.n_samples, self.n_features)
-        y = rng.randn(self.n_samples)
+        beta = rng.randn(self.n_features)
 
-        # `data` holds the keyword arguments for the `set_data` method of the
-        # objective.
-        # They are customizable.
-        return dict(X=X, y=y)
+        X = rng.randn(self.n_samples, self.n_features)
+        y = np.sign(X @ beta)
+
+        X_test = rng.randn(self.n_samples, self.n_features)
+        y_test = np.sign(X_test @ beta)
+
+        data = dict(X=X, y=y, X_test=X_test, y_test=y_test)
+
+        return 
