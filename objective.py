d4796fb28f116665659776cda4ec2b136ac4852d
Thomas Ryckeboer <thomasryck@users.noreply.github.com>
2023-10-06 16:45:22 +0200

    fix: adapt to multiclass

--- objective.py
+++ objective.py
@@ -2,12 +2,13 @@ from benchopt import BaseObjective, safe_import_context
 
 with safe_import_context() as ctx:
     import numpy as np
+    from numpy.linalg import norm 
 
 
 def _compute_loss(X, y, lmbd, beta):
     print(beta.shape)
     y_X_beta = np.expand_dims(y, axis=1) * X.dot(beta)
-    l2 = 0.5 * np.dot(beta, np.transpose(beta))
+    l2 = 0.5 * norm(beta, 2)
     return np.log1p(np.exp(-y_X_beta)).sum() + lmbd * l2
 
 
@@ -21,9 +22,10 @@ class Objective(BaseObjective):
     def __init__(self, lmbd=.1):
         self.lmbd = lmbd
 
-    def set_data(self, X, y, X_test=None, y_test=None):
+    def set_data(self, X, y, name, X_test=None, y_test=None):
         self.X, self.y = X, y
         self.X_test, self.y_test = X_test, y_test
+        self.name = name
 
     def get_one_solution(self):
         return np.zeros((self.X.shape[1]))
@@ -38,4 +40,4 @@ class Objective(BaseObjective):
         return {"value": train_loss, "Test loss": test_loss}
 
     def get_objective(self):
-        return dict(X=self.X, y=self.y, lmbd=self.lmbd)
+        return dict(X=self.X, y=self.y, lmbd=self.lmbd, name=self.name)
